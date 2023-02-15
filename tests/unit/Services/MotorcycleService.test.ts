d4a87e2aa23c2a3dfb3e81afed7cc1bfc396fdbe
Manuela França <manuelaalves26@gmail.com>
2023-02-15 17:41:20 -0300

    feat: requisito 9.

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -18,6 +18,34 @@ describe('Testes da camada MotorcycleService', function () {
     expect(createMoto).to.be.deep.equal(moto);
   });
 
+  it('testa se retorna uma lista de motocicletas com sucesso', async function () {
+    sinon.stub(Model, 'find').resolves([mocksService.motoOutput, mocksService.motoOutput]);
+   
+    const service = new MotorcycleService();  
+    const listMotos = await service.getAll();
+    
+    expect(listMotos).to.be.deep.equal([moto, moto]);
+  });
+
+  it('testa se retorna uma motocicletas pelo id com sucesso', async function () {
+    sinon.stub(Model, 'findOne').resolves(mocksService.motoOutput);
+   
+    const service = new MotorcycleService();  
+    const idMoto = await service.getById('6348513f34c397abcad040b2');
+    
+    expect(idMoto).to.be.deep.equal(moto);
+  });
+
+  it('testa se é possível atualizar uma motocicletas pelo id com sucesso', async function () {
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(mocksService.motoOutput);
+   
+    const service = new MotorcycleService();  
+    const idMoto = await service
+      .update('6348513f34c397abcad040b2', mocksService.motoInput as IMotorcycle);
+    
+    expect(idMoto).to.be.deep.equal(moto);
+  });
+
   afterEach(function () {
     sinon.restore();
   });
