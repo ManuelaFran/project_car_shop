b190188c353c083d5d738fbea19c8da7b0c248e5
Manuela França <manuelaalves26@gmail.com>
2023-02-15 11:44:01 -0300

    feat: requisito 3.

---
+++ tests/unit/Services/CarService.test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/CarService';
+import mocksService from './mocks/mocksService';
+
+const car = new Car(mocksService.carOutput);
+
+describe('Testes da camada CarService', function () {
+  // const service = new CarService();
+
+  it('testa se um carro é criado com sucesso', async function () {
+    sinon.stub(Model, 'create').resolves(mocksService.carOutput);
+   
+    const service = new CarService();  
+    const createCar = await service.create(mocksService.carInput);
+    
+    expect(createCar).to.be.deep.equal(car);
+  });
+
+  it('testa se retorna uma lista de carros com sucesso', async function () {
+    sinon.stub(Model, 'find').resolves([mocksService.carOutput, mocksService.carOutput]);
+   
+    const service = new CarService();  
+    const listCar = await service.getAll();
+    
+    expect(listCar).to.be.deep.equal([car, car]);
+  });
+
+  it('testa se retorna um carro pelo id com sucesso', async function () {
+    sinon.stub(Model, 'findOne').resolves(mocksService.carOutput);
+   
+    const service = new CarService();  
+    const idCar = await service.getById('634852326b35b59438fbea2f');
+    
+    expect(idCar).to.be.deep.equal(car);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
---
+++ tests/unit/Services/mocks/mocksService.ts
@@ -0,0 +1,22 @@
+const carOutput = {
+  id: '634852326b35b59438fbea2f',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+  
+const carInput = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export default { carOutput, carInput };
